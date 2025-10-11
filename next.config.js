ca455a150d7f608119b3d130f2a4d085060126f2
TanChu1234 <chunhattan2001@gmail.com>
2025-10-11 21:06:24 +0700

    fix: add path prefix to images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const isProduction = process.env.NODE_ENV === 'production';
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'steel-shop';
+
 const nextConfig = {
+  basePath: isProduction && isGitHubPages ? `/${repositoryName}` : '',
+  assetPrefix: isProduction && isGitHubPages ? `/${repositoryName}` : '',
   output: 'export',
   trailingSlash: true,
   images: {
