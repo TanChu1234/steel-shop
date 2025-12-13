18e41c8dda6362079c6f39ceaeab61afa03e4b27
TanChu1234 <TanChu1234@users.noreply.github.com>
2025-12-13 11:57:10 +0000

    deploy: 7112491c01a49662440e0cdeaf0f6907483d1c03

---
+++ _next/static/chunks/app/service/page-7126e17539891229.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[786,977],{5146:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,6447))}},e=>{e.O(0,[791,447,441,255,358],()=>e(e.s=5146)),_N_E=e.O()}]);
